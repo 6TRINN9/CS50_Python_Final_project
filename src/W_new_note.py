34b231ac3a8879f47a7c382f9e31f634783c56fa
Octopus <ykimaru2san@gmail.com>
2026-05-02 11:51:02 +0300

    Added window new_note and winodw generator

--- src/W_new_note.py
+++ src/W_new_note.py
@@ -6,9 +6,6 @@ from ui.ui_new_note import Ui_New_Note
 
 
 class W_new_note(QtWidgets.QDialog):
-
-    password = ""
-
     def __init__(self):       
         super(W_new_note, self).__init__() 
         self.ui = Ui_New_Note()
@@ -16,15 +13,7 @@ class W_new_note(QtWidgets.QDialog):
         self.show()
 
         self.ui.btn_create_password.clicked.connect(self.open_generator_window)
-        
+
 
     def open_generator_window(self):
-        self.w_generator = W_generator(self)
-
-    def update_info(self):
-        pass
-        
-
-    def set_password(self, password):
-        self.password = password
-        self.ui.le_password.setText(self.password)
+        self.w_generator = W_generator()
